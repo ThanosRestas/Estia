9907fedae6999e15f89c62212022c78f85107380
Thanos Restas <thanosre@gmail.com>
2020-04-09 19:04:54 +0300

    Ported the playground code into local dev environment

--- src/index.js
+++ src/index.js
@@ -1,72 +1,61 @@
-import {
-    Engine
-} from "@babylonjs/core/Engines/engine";
-import {
-    Scene
-} from "@babylonjs/core/scene";
-import {
-    Vector3
-} from "@babylonjs/core/Maths/math";
-import {
-    FreeCamera
-} from "@babylonjs/core/Cameras/freeCamera";
-import {
-    HemisphericLight
-} from "@babylonjs/core/Lights/hemisphericLight";
-import {
-    Mesh
-} from "@babylonjs/core/Meshes/mesh";
+import * as BABYLON from "@babylonjs/core/Legacy/legacy";
+import { Engine } from "@babylonjs/core/Legacy/legacy";
 
-import {
-    GridMaterial
-} from "@babylonjs/materials/grid";
-
-// Required side effects to populate the Create methods on the mesh class. Without this, the bundle would be smaller but the createXXX methods from mesh would not be accessible.
-import "@babylonjs/core/Meshes/meshBuilder";
-
-// Get the canvas element from the DOM.
 const canvas = document.getElementById("renderCanvas");
-
-// Associate a Babylon Engine to it.
 const engine = new Engine(canvas);
 
-// Create our first scene.
-var scene = new Scene(engine);
+// This creates a basic Babylon Scene object (non-mesh)
+var scene = new BABYLON.Scene(engine);
 
 // This creates and positions a free camera (non-mesh)
-var camera = new FreeCamera("camera1", new Vector3(0, 5, -10), scene);
+var camera = new BABYLON.ArcRotateCamera("Camera", 0, 0, 10, new BABYLON.Vector3(0, 0, 0), scene);
 
-// This targets the camera to scene origin
-camera.setTarget(Vector3.Zero());
+// This enables the position ( arrow ) gizmos on a mesh
+var gizmoManager = new BABYLON.GizmoManager(scene);
+
+// Enable position ( arrow ) gizmos 
+gizmoManager.positionGizmoEnabled = true;  
+
+// Create a list of meshes that can use gizmos in our scene
+gizmoManager.attachableMeshes = []; 
 
 // This attaches the camera to the canvas
 camera.attachControl(canvas, true);
 
 // This creates a light, aiming 0,1,0 - to the sky (non-mesh)
-var light = new HemisphericLight("light1", new Vector3(0, 1, 0), scene);
+var light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(0, 1, 0), scene);
 
 // Default intensity is 1. Let's dim the light a small amount
-light.intensity = 0.7;
+light.intensity = 1;
 
-// Create a grid material
-var material = new GridMaterial("grid", scene);
+// Create box.   
+var box = BABYLON.MeshBuilder.CreateBox("box", {width: 1, height: 1, depth: 1}, scene);
+// Color the box black
+var material = new BABYLON.StandardMaterial(scene);
+material.alpha = 1;
+material.diffuseColor = new BABYLON.Color3(0, 0, 0);
+box.material = material;
+// Make edges pop with color
+box.enableEdgesRendering();    
+box.edgesWidth = 4.0;
+box.edgesColor = new BABYLON.Color4(0.05, 1, 0.02);
+// Move the box upward 1/2 its height to "sit" on top of the plane
+box.position.y = box.scaling.y / 2;
 
-// Our built-in 'sphere' shape. Params: name, subdivs, size, scene
-var sphere = Mesh.CreateSphere("sphere1", 16, 2, scene);
+// Create cone
+var cone = BABYLON.MeshBuilder.CreateCylinder("cone", {diameterTop: 0, tessellation: 4}, scene);
+cone.material = material;
 
-// Move the sphere upward 1/2 its height
-sphere.position.y = 2;
+// Make edges pop with color
+cone.enableEdgesRendering();    
+cone.edgesWidth = 4.0;
+cone.edgesColor = new BABYLON.Color4(0.05, 1, 0.02);    
 
-// Affect a material
-sphere.material = material;
+// Add our box to the gizmo enabled mesh list !
+gizmoManager.attachableMeshes.push(box, cone);    
 
-// Our built-in 'ground' shape. Params: name, width, depth, subdivs, scene
-var ground = Mesh.CreateGround("ground1", 6, 6, 2, scene);
-
-// Affect a material
-ground.material = material;
-
-ground.
+// Create the floor mesh that is exempt from the gizmo list == unmovable obviously
+var ground = BABYLON.MeshBuilder.CreateGround("ground", {width: 6, height: 6}, scene);
 
 // Render every frame
 engine.runRenderLoop(() => {
