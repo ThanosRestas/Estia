d109fab88af315ca1f263bdd05cd32aaef17f8e0
nikous <nickzte@gmail.com>
2020-04-16 18:09:20 +0300

    Update:Add button,changed display of index.html

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import * as BABYLON from "@babylonjs/core/Legacy/legacy";
 import { Engine } from "@babylonjs/core/Legacy/legacy";
 
+
 const canvas = document.getElementById("renderCanvas");
 const engine = new Engine(canvas);
 
@@ -14,10 +15,10 @@ var camera = new BABYLON.ArcRotateCamera("Camera", -2.53, 0.95, 25, new BABYLON.
 var gizmoManager = new BABYLON.GizmoManager(scene);
 
 // Enable position ( arrow ) gizmos 
-gizmoManager.positionGizmoEnabled = true;  
+gizmoManager.positionGizmoEnabled = true;
 
 // Create a list of meshes that can use gizmos in our scene
-gizmoManager.attachableMeshes = []; 
+gizmoManager.attachableMeshes = [];
 
 // This attaches the camera to the canvas
 camera.attachControl(canvas, true);
@@ -33,33 +34,34 @@ var light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(0, 1, 0), 
 // Default intensity is 1. Let's dim the light a small amount
 light.intensity = 1;
 // Create the floor mesh that is exempt from the gizmo list == unmovable obviously
-var ground = BABYLON.MeshBuilder.CreateGround("ground", {width: 6, height: 6}, scene);
+var ground = BABYLON.MeshBuilder.CreateGround("ground", { width: 6, height: 6 }, scene);
 ground.checkCollisions = true;
 
 // Create boxes    
-var box = BABYLON.MeshBuilder.CreateBox("mainBox", {width: 1, height: 1, depth: 1}, scene);
+var box = BABYLON.MeshBuilder.CreateBox("mainBox", { width: 1, height: 1, depth: 1 }, scene);
 box.checkCollisions = true;
 box.position.y = 2;
 
-var box2 = BABYLON.MeshBuilder.CreateBox("positiveXaxisBox", {width: 1, height: 1, depth: 1}, scene);
+var box2 = BABYLON.MeshBuilder.CreateBox("positiveXaxisBox", { width: 1, height: 1, depth: 1 }, scene);
 box2.checkCollisions = true;
 box2.position.x = 2;
 box2.position.y = box2.scaling.y / 2;
 
-var box3 = BABYLON.MeshBuilder.CreateBox("negativeXaxisBox", {width: 1, height: 1, depth: 1}, scene);
+var box3 = BABYLON.MeshBuilder.CreateBox("negativeXaxisBox", { width: 1, height: 1, depth: 1 }, scene);
 box3.checkCollisions = true;
 box3.position.x = -2;
 box3.position.y = box3.scaling.y / 2;
 
-var box4 = BABYLON.MeshBuilder.CreateBox("positiveZaxisBox", {width: 1, height: 1, depth: 1}, scene);
+var box4 = BABYLON.MeshBuilder.CreateBox("positiveZaxisBox", { width: 1, height: 1, depth: 1 }, scene);
 box4.checkCollisions = true;
 box4.position.z = 2;
 box4.position.y = box4.scaling.y / 2;
 
-var box5 = BABYLON.MeshBuilder.CreateBox("negativeZaxisBox", {width: 1, height: 1, depth: 1}, scene);
+var box5 = BABYLON.MeshBuilder.CreateBox("negativeZaxisBox", { width: 1, height: 1, depth: 1 }, scene);
 box5.checkCollisions = true;
 box5.position.z = -2;
 box5.position.y = box5.scaling.y / 2;
+
 
 // Color the box black
 var material = new BABYLON.StandardMaterial(scene);
@@ -67,8 +69,17 @@ material.alpha = 1;
 material.diffuseColor = new BABYLON.Color3(0, 0, 0);
 box.material = material;
 
+// Button change color of the box 
+document.getElementById("btn").onclick = function () { changeColor() };
+function changeColor() {
+
+    material.diffuseColor = new BABYLON.Color3(1, 1, 1);
+
+
+}
+
 // Make edges pop with color
-box.enableEdgesRendering();    
+box.enableEdgesRendering();
 box.edgesWidth = 4.0;
 box.edgesColor = new BABYLON.Color4(0.05, 1, 0.02);
 
@@ -76,60 +87,59 @@ var sceneMeshes = [];
 sceneMeshes.push(ground, box2, box3, box4, box5);
 
 var utilLayer = new BABYLON.UtilityLayerRenderer(scene);
-var gizmoY = new BABYLON.AxisDragGizmo(new BABYLON.Vector3(0,1,0), BABYLON.Color3.FromHexString("#FFFF00"), utilLayer);
+var gizmoY = new BABYLON.AxisDragGizmo(new BABYLON.Vector3(0, 1, 0), BABYLON.Color3.FromHexString("#FFFF00"), utilLayer);
 gizmoY.attachedMesh = box;
-gizmoY.dragBehavior.onDragObservable.add((event)=>{
+gizmoY.dragBehavior.onDragObservable.add((event) => {
 
     var currentPickedObject = gizmoY.attachedMesh;
-    currentPickedObject.computeWorldMatrix();  
+    currentPickedObject.computeWorldMatrix();
     //console.log("Started dragging : " + currentPickedObject.name);
-    
-    if(collisionWithObject(currentPickedObject)){        
+
+    if (collisionWithObject(currentPickedObject)) {
         // Return the picked object to a position so that it barely touches something   
-        currentPickedObject.position.y -= event.delta.y;  
-        currentPickedObject.computeWorldMatrix();                  
+        currentPickedObject.position.y -= event.delta.y;
+        currentPickedObject.computeWorldMatrix();
     }
 })
 
-var gizmoX = new BABYLON.AxisDragGizmo(new BABYLON.Vector3(1,0,0), BABYLON.Color3.FromHexString("#e50000"), utilLayer);
+var gizmoX = new BABYLON.AxisDragGizmo(new BABYLON.Vector3(1, 0, 0), BABYLON.Color3.FromHexString("#e50000"), utilLayer);
 gizmoX.attachedMesh = box;
-gizmoX.dragBehavior.onDragObservable.add((event)=>{
+gizmoX.dragBehavior.onDragObservable.add((event) => {
 
     var currentPickedObject = gizmoX.attachedMesh;
-    currentPickedObject.computeWorldMatrix();   
+    currentPickedObject.computeWorldMatrix();
     //console.log("Started dragging : " + currentPickedObject.name);
-    
-    if(collisionWithObject(currentPickedObject)){
+
+    if (collisionWithObject(currentPickedObject)) {
         // Add/subtract the appropriate value to go to no-collision state
-         currentPickedObject.position.x -= event.delta.x;
-         // Recalibrate the position of our current picked mesh -- so collision won't happen again
-         currentPickedObject.computeWorldMatrix(); 
+        currentPickedObject.position.x -= event.delta.x;
+        // Recalibrate the position of our current picked mesh -- so collision won't happen again
+        currentPickedObject.computeWorldMatrix();
     }
 })
 
-var gizmoZ = new BABYLON.AxisDragGizmo(new BABYLON.Vector3(0,0,1), BABYLON.Color3.FromHexString("#0000ff"), utilLayer);
+var gizmoZ = new BABYLON.AxisDragGizmo(new BABYLON.Vector3(0, 0, 1), BABYLON.Color3.FromHexString("#0000ff"), utilLayer);
 gizmoZ.attachedMesh = box;
-gizmoZ.dragBehavior.onDragObservable.add((event)=>{
+gizmoZ.dragBehavior.onDragObservable.add((event) => {
 
     // Getting the current picked mesh
     var currentPickedObject = gizmoZ.attachedMesh;
     // Linear algebra black magic I will explain asap || basically recalibrating
     // the position of our object in space
-    currentPickedObject.computeWorldMatrix();    
-    
+    currentPickedObject.computeWorldMatrix();
+
     // On collision with the list of available meshes stop movement
-    if(collisionWithObject(currentPickedObject)){
+    if (collisionWithObject(currentPickedObject)) {
         // Add/subtract the appropriate value to go to no-collision state
         currentPickedObject.position.z -= event.delta.z;
         // Recalibrate the position of our current picked mesh -- so collision won't happen again
-        currentPickedObject.computeWorldMatrix();                        
+        currentPickedObject.computeWorldMatrix();
     }
 })
 
-function collisionWithObject(mesh)
-{
-    for (let i = 0; i < sceneMeshes.length; i++ ){
-        if(mesh.intersectsMesh(sceneMeshes[i])){
+function collisionWithObject(mesh) {
+    for (let i = 0; i < sceneMeshes.length; i++) {
+        if (mesh.intersectsMesh(sceneMeshes[i])) {
             console.log(mesh.name + " collides with : " + sceneMeshes[i].name);
             return true;
         }
@@ -138,27 +148,27 @@ function collisionWithObject(mesh)
 }
 
 
- // Key controller
- scene.onKeyboardObservable.add((kbInfo) => {
+// Key controller
+scene.onKeyboardObservable.add((kbInfo) => {
     switch (kbInfo.type) {
-    case BABYLON.KeyboardEventTypes.KEYDOWN:
-        switch (kbInfo.event.key) {
-        // The button to enable the BabylonJS Inspector   
-        case "`":
-            console.log("Debug mode enabled");           
-            if(debug){
-                scene.debugLayer.hide();
-                debug = false;
-            }
-            else{
-                scene.debugLayer.show();
-                debug = true;
+        case BABYLON.KeyboardEventTypes.KEYDOWN:
+            switch (kbInfo.event.key) {
+                // The button to enable the BabylonJS Inspector   
+                case "`":
+                    console.log("Debug mode enabled");
+                    if (debug) {
+                        scene.debugLayer.hide();
+                        debug = false;
+                    }
+                    else {
+                        scene.debugLayer.show();
+                        debug = true;
+                    }
+                    break;
             }
             break;
-        }                
-        break;
     }
- });
+});
 
 // Render every frame
 engine.runRenderLoop(() => {
