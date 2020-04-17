fc7ee34535a0b2eaf250de294ca0a0575b7f9ded
Thanos Restas <thanosre@gmail.com>
2020-04-18 01:46:11 +0300

    Refactor: Created reusable gizmo class creator

--- src/item.js
+++ src/item.js
@@ -1,5 +1,7 @@
 export default class Item{
     constructor(mesh){
-        this.item = mesh;
+        console.log("Item is created");
+        this.name = mesh.name;
+        this.mesh = mesh;
     }
 }
