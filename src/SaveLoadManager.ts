91b30fa9118260d0b8033475c8c4857777d5db33
Thanos Restas <thanosre@gmail.com>
2020-05-05 14:54:28 +0300

    Refactor : SaveLoadManager class

--- src/SaveLoadManager.ts
+++ src/SaveLoadManager.ts
@@ -8,80 +8,78 @@ import { TransformNode } from "@babylonjs/core/Legacy/legacy";
 export const loadButton = document.getElementById("loadFile");
 export const saveButton = document.getElementById("saveScene");
 export const deleteButton = document.getElementById("delete");
-let objectUrl: string;
 
 
-saveButton.onclick = function ()
+class SaveLoadManager
 {
-  if (confirm('Do you want to download that scene?'))
+  static save()
   {
-    doDownload('scene', scene);
-  } else
-  {
-    // Do nothing!
-    console.log('Save canceled.');
-  }
-};
+    let objectUrl: string;
+    let filename = 'scene';
 
-loadButton.onchange = function (evt)
-{
-  const files = (<HTMLInputElement>evt.target).files;
-  const filename = files[0].name;
-  const filenameLowercase = filename.toLowerCase();
-  const blob = new Blob([files[0]]);
-
-  BABYLON.FilesInput.FilesToLoad[filenameLowercase] = blob as File;
-
-  let task = assetsManager.addMeshTask('task1', '', 'file:', filenameLowercase);
-  assetsManager.load();
-  task.onSuccess = function (task)
-  {
-    // Temporary parent for non parented meshes for easy gizmo pick
-    let parentTemp = new BABYLON.TransformNode("parentTemp");
-    task.loadedMeshes.forEach(mesh =>
+    if (objectUrl)
     {
-      pickableMeshes.push(mesh);      
-      // Assign temp parent
-      mesh.parent = parentTemp;
-    });
+      window.URL.revokeObjectURL(objectUrl);
+    }
+
+    var serializedScene = BABYLON.SceneSerializer.Serialize(scene);
+
+    var strMesh = JSON.stringify(serializedScene);
+    // var strMesh = safeJsonStringify(serializedScene);
+
+    if (filename.toLowerCase().lastIndexOf('.babylon') !== filename.length - 8 || filename.length < 9)
+    {
+      filename += '.babylon';
+    }
+
+    var blob = new Blob([strMesh], { type: 'octet/stream' });
+
+    // turn blob into an object URL; saved as a member, so can be cleaned out later
+    objectUrl = (window.webkitURL || window.URL).createObjectURL(blob);
+
+    var link = window.document.createElement('a');
+    link.href = objectUrl;
+    link.download = filename;
+    var click = document.createEvent('MouseEvents');
+    click.initEvent('click', true, false);
+    link.dispatchEvent(click);
   }
-};
 
 
-deleteButton.onclick = function ()
-{
-  ActiveEntityManager.currentActiveMesh.dispose();
-  ActiveEntityManager.currentActiveGizmo.disable();
-  console.log("Test");
-};
-
-
-function doDownload(filename, scene)
-{
-  if (objectUrl)
+  static load(this : HTMLElement, evt : Event )
   {
-    window.URL.revokeObjectURL(objectUrl);
+    const files = (<HTMLInputElement>evt.target).files;
+    const filename = files[0].name;
+    const filenameLowercase = filename.toLowerCase();
+    const blob = new Blob([files[0]]);
+
+    BABYLON.FilesInput.FilesToLoad[filenameLowercase] = blob as File;
+
+    let task = assetsManager.addMeshTask('task1', '', 'file:', filenameLowercase);
+    assetsManager.load();
+    task.onSuccess = function (task)
+    {
+      // Temporary parent for non parented meshes for easy gizmo pick
+      let parentTemp = new BABYLON.TransformNode("parentTemp");
+      task.loadedMeshes.forEach(mesh =>
+      {
+        pickableMeshes.push(mesh);
+        // Assign temp parent
+        mesh.parent = parentTemp;
+      });
+    }
   }
 
-  var serializedScene = BABYLON.SceneSerializer.Serialize(scene);
-
-  var strMesh = JSON.stringify(serializedScene);
-  // var strMesh = safeJsonStringify(serializedScene);
-
-  if (filename.toLowerCase().lastIndexOf('.babylon') !== filename.length - 8 || filename.length < 9)
+  static delete()
   {
-    filename += '.babylon';
+    ActiveEntityManager.currentActiveMesh.dispose();
+    ActiveEntityManager.currentActiveGizmo.disable();
+    console.log("Test");
   }
-
-  var blob = new Blob([strMesh], { type: 'octet/stream' });
-
-  // turn blob into an object URL; saved as a member, so can be cleaned out later
-  objectUrl = (window.webkitURL || window.URL).createObjectURL(blob);
-
-  var link = window.document.createElement('a');
-  link.href = objectUrl;
-  link.download = filename;
-  var click = document.createEvent('MouseEvents');
-  click.initEvent('click', true, false);
-  link.dispatchEvent(click);
 }
+
+deleteButton.addEventListener("click", SaveLoadManager.delete);
+loadButton.addEventListener("change", SaveLoadManager.load);
+saveButton.addEventListener("click", SaveLoadManager.save);
+
+
