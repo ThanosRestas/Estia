8274b2c19f50ae8bddc6019a19c7be33de9b7902
Thanos Restas <thanosre@gmail.com>
2020-05-06 23:06:44 +0300

    Refactor : Fixed lint errors

--- src/ActiveEntityManager.ts
+++ src/ActiveEntityManager.ts
@@ -2,10 +2,10 @@ import * as BABYLON from '@babylonjs/core/Legacy/legacy';
 import CustomGizmo from './GizmoManager';
 
 export default class ActiveEntityManager {
-    static currentActiveMesh : BABYLON.Mesh;
-    static currentActiveGizmo : CustomGizmo;
-    static positionGizmoActive : Boolean = false;
-    static rotationGizmoActive  : Boolean = false;
-    static scaleGizmoActive  : Boolean = false;
-    static debug : Boolean = false;
+    static currentActiveMesh: BABYLON.Mesh;
+    static currentActiveGizmo: CustomGizmo;
+    static positionGizmoActive = false;
+    static rotationGizmoActive = false;
+    static scaleGizmoActive = false;
+    static debug = false;
 }
